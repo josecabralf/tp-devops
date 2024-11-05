380eb7dcabc2fd388c51210ba723ca15d3c49060
cabralfbenja <cabralbenja2001@gmail.com>
2024-11-04 21:48:33 -0300

    85829 - eslint web

---
+++ web/eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+module.exports = [
+  {
+    ignores: ["node_modules"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+    },
+  },
+];

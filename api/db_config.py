2f4335829de7447a1edb5ec916a454c92311c718
cabralfbenja <cabralbenja2001@gmail.com>
2024-11-07 19:49:36 -0300

    85829 - Dockerfiles

--- api/db_config.py
+++ api/db_config.py
@@ -1,12 +1,17 @@
 from app import app
 from flaskext.mysql import MySQL
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] =  os.environ.get('MYSQL_DATABASE_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] =  os.environ.get('MYSQL_DATABASE_DB')
-app.config['MYSQL_DATABASE_HOST'] =  os.environ.get('MYSQL_DATABASE_HOST')  or 'localhost'
-app.config['MYSQL_DATABASE_PORT'] =  int(os.environ.get('MYSQL_DATABASE_PORT')) or 3306
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST', 'localhost')
+app.config['MYSQL_DATABASE_PORT'] = int(os.environ.get('MYSQL_DATABASE_PORT', 3306))
+
 print(app.config)
 mysql.init_app(app)

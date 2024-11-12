c3f3aab58ab0e56ad8e2f65058d9c0e0851d632d
José Cabral <cabraljose1210@gmail.com>
2024-11-12 03:36:26 -0300

    JIC: secrets to compose

--- api/db_config.py
+++ api/db_config.py
@@ -1,10 +1,13 @@
+import os
 from app import app
 from flaskext.mysql import MySQL
+
+
 mysql = MySQL()
- 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'testuser'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'testpass'
-app.config['MYSQL_DATABASE_DB'] = 'utn'
-app.config['MYSQL_DATABASE_HOST'] = 'db-crud-users'
+
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST', 'localhost')
+
 mysql.init_app(app)

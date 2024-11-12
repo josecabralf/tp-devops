78aad5d9d0cb94d114beeb694075c1d144aee4c0
Benjamin <benjagarate1@gmail.com>
2024-11-12 16:26:50 -0300

    fix output

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Realizar el POST
-echo "Realizando POST a /api/add" >> $HEALTHCHECK_OUTPUT
+OUTPUT_FILE=./healthcheck.log
+
+echo "\n----------------- API-PYTHON POST ------------------\n" >> $OUTPUT_FILE
+echo "Realizando POST a /api/add" >> $OUTPUT_FILE
 curl -X POST http://127.0.0.1:8080/api/add \
 -H "Content-Type: application/json" \
--d '{"name": "John Doe", "email": "johndoe@example.com", "pwd": "yourpassword"}' >> $HEALTHCHECK_OUTPUT 2>&1
-echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+-d '{"name": "John Doe", "email": "johndoe@example.com", "pwd": "yourpassword"}' >> $OUTPUT_FILE 2>&1
+echo "\n-----------------------------------\n" >> $OUTPUT_FILE
 
-# Realizar el GET a /api/users
-echo "Realizando GET a /api/users" >> $HEALTHCHECK_OUTPUT
-curl -X GET http://127.0.0.1:8080/api/users >> $HEALTHCHECK_OUTPUT 2>&1
-echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+echo "\n---------------- API-PYTHON GET -------------------\n" >> $OUTPUT_FILE
+echo "Realizando GET a /api/users" >> $OUTPUT_FILE
+curl -X GET http://127.0.0.1:8080/api/users >> $OUTPUT_FILE 2>&1
+echo "\n-----------------------------------\n" >> $OUTPUT_FILE
 
-# Realizar el GET a /api/hello-world
-echo "Realizando GET a /api/hello-world" >> $HEALTHCHECK_OUTPUT
-curl -X GET http://127.0.0.1:8080/api/hello-world >> $HEALTHCHECK_OUTPUT 2>&1
-echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+echo "\n--------------- API-NODE --------------------\n" >> $OUTPUT_FILE
+echo "Realizando GET a /api/hello-world" >> $OUTPUT_FILE
+curl -X GET http://127.0.0.1:8080/api/hello-world >> $OUTPUT_FILE 2>&1
+echo "\n-----------------------------------\n" >> $OUTPUT_FILE
 
-# Realizar el GET a http://127.0.0.1:3000/
-echo "Realizando GET a http://127.0.0.1:3000/" >> $HEALTHCHECK_OUTPUT
-curl -X GET http://127.0.0.1:3000/ >> $HEALTHCHECK_OUTPUT 2>&1
-echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+echo "\n---------------- REACT -------------------\n" >> $OUTPUT_FILE
+echo "Realizando GET a http://127.0.0.1:3000/" >> $OUTPUT_FILE
+curl -X GET http://127.0.0.1:3000/ >> $OUTPUT_FILE 2>&1
+echo "\n-----------------------------------\n" >> $OUTPUT_FILE

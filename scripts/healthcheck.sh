fdc990dab62822f985b79dacd01bd7be518c8605
Benjamin <benjagarate1@gmail.com>
2024-11-12 16:20:02 -0300

    healtcheck with curls

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-# healthcheck.sh
 
-docker ps
+# Realizar el POST
+echo "Realizando POST a /api/add" >> $HEALTHCHECK_OUTPUT
+curl -X POST http://127.0.0.1:8080/api/add \
+-H "Content-Type: application/json" \
+-d '{"name": "John Doe", "email": "johndoe@example.com", "pwd": "yourpassword"}' >> $HEALTHCHECK_OUTPUT 2>&1
+echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+
+# Realizar el GET a /api/users
+echo "Realizando GET a /api/users" >> $HEALTHCHECK_OUTPUT
+curl -X GET http://127.0.0.1:8080/api/users >> $HEALTHCHECK_OUTPUT 2>&1
+echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+
+# Realizar el GET a /api/hello-world
+echo "Realizando GET a /api/hello-world" >> $HEALTHCHECK_OUTPUT
+curl -X GET http://127.0.0.1:8080/api/hello-world >> $HEALTHCHECK_OUTPUT 2>&1
+echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT
+
+# Realizar el GET a http://127.0.0.1:3000/
+echo "Realizando GET a http://127.0.0.1:3000/" >> $HEALTHCHECK_OUTPUT
+curl -X GET http://127.0.0.1:3000/ >> $HEALTHCHECK_OUTPUT 2>&1
+echo "\n-----------------------------------\n" >> $HEALTHCHECK_OUTPUT

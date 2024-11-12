a9a7b4724354dd9e01403a0e1a857879c0f1e1d5
Benjamin <benjagarate1@gmail.com>
2024-11-12 16:42:55 -0300

    espacios

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -3,29 +3,25 @@
 OUTPUT_FILE=./healthcheck.log
 
 echo "API-PYTHON POST" >> $OUTPUT_FILE
-
 echo "Realizando POST a /api/add" >> $OUTPUT_FILE
 curl -X POST http://127.0.0.1:8080/api/add \
 -H "Content-Type: application/json" \
 -d '{"name": "John Doe", "email": "johndoe@example.com", "pwd": "yourpassword"}' >> $OUTPUT_FILE 2>&1
 
-
-
+echo "" >> $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
 echo "API-PYTHON GET" >> $OUTPUT_FILE
-
 echo "Realizando GET a /api/users" >> $OUTPUT_FILE
 curl -X GET http://127.0.0.1:8080/api/users >> $OUTPUT_FILE 2>&1
 
-
-
+echo "" >> $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
 echo "API-NODE" >> $OUTPUT_FILE
-
 echo "Realizando GET a /api-node/hello-world" >> $OUTPUT_FILE
 curl -X GET http://127.0.0.1:8080/api-node/hello-world >> $OUTPUT_FILE 2>&1
 
-
-
+echo "" >> $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
 echo "REACT" >> $OUTPUT_FILE
-
 echo "Realizando GET a http://127.0.0.1:3000/" >> $OUTPUT_FILE
 curl -X GET http://127.0.0.1:3000/ >> $OUTPUT_FILE 2>&1
